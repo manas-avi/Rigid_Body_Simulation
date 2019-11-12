resolved derivative bug

PrismaticJoint.py
@@ -38,7 +38,7 @@ class PrismaticJoint(object):
 		self.Ic = np.zeros((3,3))
 		# no rotation component associated
 
-		self.showText = True
+		self.showText = False
 		self.showCom = True
 
 		self.dq = np.array([0])

RevoluteJoint.py
@@ -3,10 +3,10 @@ import pdb
 from special_matrices import *
 from scipy.spatial.transform import Rotation as R
 
-class Rod1_3D(object):
-	"""docstring for Rod1_3D"""
+class RevoluteJoint(object):
+	"""docstring for RevoluteJoint"""
 	def __init__(self, name, x1, y1,z1, x2, y2,z2, x_length, x_alpha, z_length, color=None, typ=None):
-		super(Rod1_3D, self).__init__()
+		super(RevoluteJoint, self).__init__()
 		self.index = 0
 		self.type = 'RevoluteJoint'
 		self.name = name
@@ -45,7 +45,7 @@ class Rod1_3D(object):
 		self.Ic[1,1] = (1/12) * self.mass * (self.length**2)
 		self.Ic[2,2] = (1/12) * self.mass * (self.length**2)
 
-		self.showText = True
+		self.showText = False
 		self.showCom = True
 
 		self.dq = np.array([0])

contact_handling.py
@@ -2,7 +2,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import patches
 from matplotlib.collections import PatchCollection
 import numpy as np
-import RevoluteJoint
+import Rod1_3D
 import PrismaticJoint
 import matplotlib.pyplot as plt
 import pdb
@@ -30,7 +30,6 @@ def evaluate_potential_energy(obj_list):
 	for i in range(1,n+1):
 		matrix = matrix @ obj_list[i-1].get_transformation_matrix() 
 		O[i] = (matrix @ np.append([O[0]],1))[0:3]
-		
 
 	for i in range(n):
 		mi = obj_list[i].getMass()
@@ -243,8 +242,8 @@ def detectCollision(obj):
 	else:
 		return False
 
-# g = np.array([0,0,-10])
 g = np.array([0,0,-10])
+# g = np.array([0,0,-10])
 # same axis of rotation
 origin=np.array([4,4,1,1], dtype=np.float32)
 
@@ -252,17 +251,16 @@ origin=np.array([4,4,1,1], dtype=np.float32)
 # pobj_0.addChild(pobj_1)
 
 
-pobj_0 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,0, 4,4,0, 	q_angle=0, x_alpha=0,r_length=0, color="red")
-# pobj_0 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,0, 4,4,0, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="red")
-# pobj_1 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,0, 4,4,0, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="blue")
-# pobj_2 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,0, 4,4,0, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="green")
+pobj_0 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,0, 4,4,0, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="red")
+pobj_1 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,0, 4,4,0, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="blue")
+pobj_2 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,0, 4,4,0, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="green")
 
-# obj_0 = RevoluteJoint.RevoluteJoint('obj_0', 4,4,1, 4,4,1, x_length=0, x_alpha=np.pi/2,z_length=0, color="red")
+# obj_0 = Rod1_3D.Rod1_3D('obj_0', 4,4,1, 4,4,1, x_length=0, x_alpha=np.pi/2,z_length=0, color="red")
 
-obj_1 = RevoluteJoint.RevoluteJoint('obj_1', 4,4,1, 6,4,1, x_length=2, x_alpha=0,z_length=0, color="blue")
+obj_1 = Rod1_3D.Rod1_3D('obj_1', 4,4,1, 6,4,1, x_length=2, x_alpha=0,z_length=0, color="blue")
 pobj_0.addChild(obj_1)
 
-obj_2 = RevoluteJoint.RevoluteJoint('obj_2', 6,4,1, 8,4,1, x_length=2, x_alpha=0,z_length=0, color="green")
+obj_2 = Rod1_3D.Rod1_3D('obj_2', 6,4,1, 8,4,1, x_length=2, x_alpha=0,z_length=0, color="green")
 obj_1.addChild(obj_2)
 
 
@@ -297,7 +295,7 @@ for i in range(n):
 
 
 if __name__ == '__main__':
-	dt = 0.001
+	dt = 0.01
 	q = np.zeros((n))
 	dqdt = np.zeros((n))
 	d2qdt2 = np.zeros((n))
@@ -372,13 +370,12 @@ if __name__ == '__main__':
 					links.append(obj.child[i])
 		# pdb.set_trace()
 		# print("t: ", np.array([t]), " q : ", q * 180 / np.pi % 360, "dqdt : " , dqdt, "d2qdt2 : ", d2qdt2, "rhs : ", rhs, "ke : ", np.array([ke]), "pe : ", np.array([pe]))
-		# print("t: ", np.array([t]), "d2qdt2 : ", d2qdt2)
-		print("t: ", np.array([t]), "ke : ", np.array([ke]))
-		print("t: ", np.array([t]), "pe : ", np.array([pe]))
-		# print("t: ", np.array([t]), "Dq : ", Dq)
-		# print("t: ", np.array([t]), "C : ", C)
-		# print("t: ", np.array([t]), "Phi : ", phi)
-		# print()
+		print("t: ", np.array([t]), "d2qdt2 : ", d2qdt2)
+		print("t: ", np.array([t]), "energy : ", np.array([ke+pe]))
+		print("t: ", np.array([t]), "Dq : ", Dq)
+		print("t: ", np.array([t]), "C : ", C)
+		print("t: ", np.array([t]), "Phi : ", phi)
+		print()
 		# t_list.append(t)
 		# e_list.append(ke+pe)
 		# q_list.append(q[0])

special_matrices.py
@@ -75,4 +75,4 @@ def getVector3X(vector4X):
 	return np.array([vector4X[0], vector4X[1], vector4X[2]]) / vector4X[3]
 
 def runge_kutta_method(v, vdot, dt):
-	pass
+	pass	