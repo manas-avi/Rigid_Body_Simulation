resolved bug in getting Dq derivative

non_moving_com.py
@@ -227,6 +227,10 @@ def evaluate_Dq_derivative(obj_list):
 				(np.transpose(Jwi) @ Ri @ Ii @ np.transpose(Rdi) @ Jwi) 
 				+ (np.transpose(dJwi) @ Ri @ Ii @ np.transpose(Ri) @ Jwi) + \
 				(np.transpose(Jwi) @ Ri @ Ii @ np.transpose(Ri) @ dJwi)
+			# Dq += np.transpose(Jwi) @ Ri @ Ii @ np.transpose(Ri) @ Jwi
+			# if k==0:
+			# 	print(C[:,:,k])
+			# 	pdb.set_trace()
 	return C
 
 def createCArray(Dq_derivative, dqdt):
@@ -300,11 +304,7 @@ def detectCollision(obj):
 	else:
 		return False
 
-# g = np.array([0,0,-10])
-# g = np.array([0,-10,0])
-# g = np.array([-10,0,0])
-# g = np.array([-1,-1,-1])
-g = np.array([0,0,0])
+g = np.array([0,0,-10])
 # same axis of rotation
 origin=np.array([4,4,1,1], dtype=np.float32)
 # Always ensure that stating axis is always inclined with cartesian axis
@@ -322,7 +322,6 @@ pobj_0.addChild(pobj_1)
 pobj_1.addChild(pobj_2)
 
 obj_1 = RevoluteJoint.RevoluteJoint('obj_1', 4,4,1, 6,4,1, x_length=2, x_alpha=0,z_length=0, color="pink")
-# obj_1 = RevoluteJoint.RevoluteJoint('obj_1', 4,4,1, 6,4,1, x_length=2, x_alpha=np.pi/2,z_length=0, color="violet")
 obj_1.showText = True
 pobj_2.addChild(obj_1)
 
@@ -332,10 +331,7 @@ obj_1.addChild(obj_2)
 
 
 # ground is the x-y plane
-# obj_list = [pobj_0]
-# obj_list = [pobj_0,pobj_1,pobj_2, obj_1]
 obj_list = [pobj_0,pobj_1,pobj_2, obj_1, obj_2]
-# obj_list = [obj_1, obj_2]
 n = len(obj_list)
 
 # set Axis for all the joints ----
@@ -352,11 +348,7 @@ for i in range(0,n):
 	obj_list[i].set_yaxis(axis_y)
 	obj_list[i].set_zaxis(axis_z)
 
-# torque = np.array([0], dtype=np.float32)
-# torque = np.array([0, 0], dtype=np.float32)
-# torque = np.array([1, 1], dtype=np.float32)
 torque = np.zeros((n,))
-# torque = np.array([0,0, 0, 0,0], dtype=np.float32)
 
 # set indices 
 for i in range(n):
@@ -383,23 +375,10 @@ if __name__ == '__main__':
 		dqdt[i] = obj_list[i].getDq()
 		d2qdt2[i] = obj_list[i].getD2q()
 
-	# dqdt = np.array([5], dtype=np.float32)
-	# dqdt = np.array([10,1], dtype=np.float32)
-	# dqdt = np.array([0,0,2], dtype=np.float32)
-	# dqdt = np.array([0,-2,2], dtype=np.float32)
-	# dqdt = np.array([0,0,-2,2], dtype=np.float32)
-	# dqdt = np.array([0,0,0,2], dtype=np.float32)
 	dqdt = np.array([0,0,1,-1,2], dtype=np.float32)
 
-	# t_list=[]
-	# e_list=[]
-	# q_list=[]
 	while True:
 
-		# if t<100*dt:
-		# 	torque[4] = 5
-		# else:
-		# 	torque[4] = 0
 		rhs = torque.copy()
 		ke = 0
 		Dq =  evaluate_Dq(obj_list)
@@ -462,26 +441,10 @@ if __name__ == '__main__':
 			else:
 				for i in range(len(obj.child)):
 					links.append(obj.child[i])
-		# pdb.set_trace()
-		# print("t: ", np.array([t]), " q : ", q, "dqdt : " , dqdt, "d2qdt2 : ", d2qdt2, "rhs : ", rhs, "ke : ", np.array([ke]), "pe : ", np.array([pe]))
-		# print("t: ", np.array([t]), "d2qdt2 : ", d2qdt2)
-		# print("t: ", np.array([t]), "dqdt : ", dqdt)
-		# print("t: ", np.array([t]), "q : ", q)
+
 		print("t: ", np.array([t]), "energy : ", np.array([ke+pe]))
 		print("t: ", np.array([t]), "Angular momentum : ", '\n',  Dq@dqdt, np.sum(Dq@dqdt))
-		# print("t: ", np.array([t]), " Dq : ", '\n',  Dq)
-		# print("t: ", np.array([t]), "C : ", C)	
-		# print("t: ", np.array([t]), "Phi : ", phi)
-		# print("t: ", np.array([t]), "rhs : ", rhs)
 		print()
-
-		# debug statemetns
-		# t_list.append(t)
-		# q1_list.append(q[0])
-		# q2_list.append(q[1])
-
-		# e_list.append(ke+pe)
-		# q_list.append(q[0])
 
 		# render object
 		for artist in plt.gca().lines + plt.gca().collections + plt.gca().texts:
@@ -489,16 +452,6 @@ if __name__ == '__main__':
 
 		for i in range(n):
 			obj_list[i].drawObject(ax)
-
-
-		# debug code ----------------------------------
-		# O = np.zeros((n+1,3))
-		# matrix = np.eye(4)
-		# for i in range(1,n+1):
-		# 	matrix = matrix @obj_list[i-1].get_transformation_matrix() 
-		# 	O[i] = (matrix @ np.append([O[0]],1))[0:3]
-		# 	ax.plot( [O[i-1][0], O[i][0]] ,[O[i-1][1], O[i][1]], [O[i-1][2], O[i][2]], marker = '*', color='red')
-		# debug code ends ------------------------------
 
 		# draw ground at xy plane
 		xx, yy = np.meshgrid(range(0,11,10), range(0,11,10))
@@ -516,24 +469,3 @@ if __name__ == '__main__':
 
 		fig.canvas.draw()
 		fig.canvas.flush_events()
-
-		# if t>10:
-		# 	break
-
-
-# plt.title('energy graph')
-# plt.xlabel('time ---->')
-# plt.ylabel('energy ---->')
-# # plt.plot(t_list, e_list)
-# plt.plot(t_list, q1_list, color='r')
-# plt.plot(t_list, q2_list, color='b')
-# plt.show()
-
-# Notes --------------------------------
-
-# this is for revolute joint change at this point for prismatic joint
-# there is some problem with this one
-# since this value is dependent on theta which is quadratically integerated
-# thus its value depend on how small dt is
-# thus not giving an exact answer since area intergerated is not correct.
-# since get transformation matrix depends on the value of current theta

rod_falling_under_gravity.py
@@ -300,11 +300,11 @@ def detectCollision(obj):
 	else:
 		return False
 
-# g = np.array([0,0,-10])
+g = np.array([0,0,-10])
 # g = np.array([0,-10,0])
 # g = np.array([-10,0,0])
 # g = np.array([-1,-1,-1])
-g = np.array([0,0,0])
+# g = np.array([0,0,0])
 # same axis of rotation
 origin=np.array([4,4,1,1], dtype=np.float32)
 # Always ensure that stating axis is always inclined with cartesian axis
@@ -312,29 +312,41 @@ origin=np.array([4,4,1,1], dtype=np.float32)
 # pobj_0.addChild(pobj_1)
 
 
+# pobj_0 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,1, 4,4,1, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="red")
+# pobj_0.setMass(1e-10)
+# pobj_1 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,1, 4,4,1, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="blue")
+# pobj_1.setMass(1e-10)
+# pobj_2 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,1, 4,4,1, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="green")
+# pobj_2.setMass(1e-10)
+# pobj_0.addChild(pobj_1)
+# pobj_1.addChild(pobj_2)
+
 pobj_0 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,1, 4,4,1, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="red")
 pobj_0.setMass(0)
-pobj_1 = PrismaticJoint.PrismaticJoint('pobj_1', 4,4,1, 4,4,1, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="blue")
+# pobj_0.showText = True
+pobj_1 = PrismaticJoint.PrismaticJoint('pobj_0', 4,4,1, 4,4,1, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="blue")
 pobj_1.setMass(0)
-pobj_2 = PrismaticJoint.PrismaticJoint('pobj_2', 4,4,1, 4,4,1, 	q_angle=np.pi/2, x_alpha=np.pi/2,r_length=0, color="green")
-pobj_2.setMass(0)
-pobj_0.addChild(pobj_1)
-pobj_1.addChild(pobj_2)
+# pobj_1.showText = True
+# pobj_1.setMass(1e-10)
 
 obj_1 = RevoluteJoint.RevoluteJoint('obj_1', 4,4,1, 6,4,1, x_length=2, x_alpha=0,z_length=0, color="pink")
-# obj_1 = RevoluteJoint.RevoluteJoint('obj_1', 4,4,1, 6,4,1, x_length=2, x_alpha=np.pi/2,z_length=0, color="violet")
+# obj_1 = RevoluteJoint.RevoluteJoint('obj_1', 4,4,1, 6,4,1, x_length=2, x_alpha=0,z_length=0, color="violet")
 obj_1.showText = True
-pobj_2.addChild(obj_1)
+pobj_0.addChild(pobj_1)
+pobj_1.addChild(obj_1)
+# pobj_2.addChild(obj_1)
 
-obj_2 = RevoluteJoint.RevoluteJoint('obj_2', 6,4,1, 8,4,1, x_length=2, x_alpha=0,z_length=0, color="cyan")
-obj_2.showText = True
-obj_1.addChild(obj_2)
+# obj_2 = RevoluteJoint.RevoluteJoint('obj_2', 6,4,1, 8,4,1, x_length=2, x_alpha=0,z_length=0, color="cyan")
+# obj_2.showText = True
+# obj_1.addChild(obj_2)
 
 
 # ground is the x-y plane
 # obj_list = [pobj_0]
+obj_list = [pobj_0, pobj_1, obj_1]
+# obj_list = [pobj_0, pobj_1, pobj_2]
 # obj_list = [pobj_0,pobj_1,pobj_2, obj_1]
-obj_list = [pobj_0,pobj_1,pobj_2, obj_1, obj_2]
+# obj_list = [pobj_0,pobj_1,pobj_2, obj_1, obj_2]
 # obj_list = [obj_1, obj_2]
 n = len(obj_list)
 
@@ -386,10 +398,9 @@ if __name__ == '__main__':
 	# dqdt = np.array([5], dtype=np.float32)
 	# dqdt = np.array([10,1], dtype=np.float32)
 	# dqdt = np.array([0,0,2], dtype=np.float32)
-	# dqdt = np.array([0,-2,2], dtype=np.float32)
-	# dqdt = np.array([0,0,-2,2], dtype=np.float32)
-	# dqdt = np.array([0,0,0,2], dtype=np.float32)
-	dqdt = np.array([0,0,1,-1,2], dtype=np.float32)
+	dqdt = np.array([0,-2,2], dtype=np.float32)
+	# dqdt = np.array([0,0,0,1], dtype=np.float32)
+	# dqdt = np.array([0,0,0,1,0], dtype=np.float32)
 
 	# t_list=[]
 	# e_list=[]
@@ -416,6 +427,9 @@ if __name__ == '__main__':
 			rhs[k] = rhs[k] - phi[k] - C[k]
 
 		d2qdt2 = np.linalg.solve(Dq, rhs)
+		# d2qdt2[0] = 0
+		# d2qdt2[1] = 0
+		# d2qdt2[2] = 0
 		# d2qdt2 = fixed_point_method(torque, dt, obj_list)
 
 			# # detect collisions - 
@@ -464,18 +478,18 @@ if __name__ == '__main__':
 					links.append(obj.child[i])
 		# pdb.set_trace()
 		# print("t: ", np.array([t]), " q : ", q, "dqdt : " , dqdt, "d2qdt2 : ", d2qdt2, "rhs : ", rhs, "ke : ", np.array([ke]), "pe : ", np.array([pe]))
-		# print("t: ", np.array([t]), "d2qdt2 : ", d2qdt2)
-		# print("t: ", np.array([t]), "dqdt : ", dqdt)
-		# print("t: ", np.array([t]), "q : ", q)
-		print("t: ", np.array([t]), "energy : ", np.array([ke+pe]))
-		print("t: ", np.array([t]), "Angular momentum : ", '\n',  Dq@dqdt, np.sum(Dq@dqdt))
+		print("t: ", np.array([t]), "q : ", q)
+		print("t: ", np.array([t]), "dqdt : ", dqdt)
+		print("t: ", np.array([t]), "d2qdt2 : ", d2qdt2)
+		print()
+		# print("t: ", np.array([t]), "energy : ", np.array([ke+pe]))
+		# print("t: ", np.array([t]), "Angular momentum : ", '\n',  Dq@dqdt, np.sum(Dq@dqdt))
 		# print("t: ", np.array([t]), " Dq : ", '\n',  Dq)
-		# print("t: ", np.array([t]), "C : ", C)	
+		# print("t: ", np.array([t]), "C : ", C)
 		# print("t: ", np.array([t]), "Phi : ", phi)
 		# print("t: ", np.array([t]), "rhs : ", rhs)
-		print()
-
-		# debug statemetns
+		# print()
+		# pdb.set_trace()
 		# t_list.append(t)
 		# q1_list.append(q[0])
 		# q2_list.append(q[1])